72094229b9bad24523f652d0e3159eb8b655a6f8
Hossain Ayman <ayman.dev2003@gmail.com>
2026-07-02 00:18:40 +0600

    Resume Spring REST API section

--- src/main/java/com/employeeManagementCRUD/EmployeeDirectory/EmployeeDirectoryApplication.java
+++ src/main/java/com/employeeManagementCRUD/EmployeeDirectory/EmployeeDirectoryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeDirectoryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("no datasource configured");
 		SpringApplication.run(EmployeeDirectoryApplication.class, args);
 	}
 
