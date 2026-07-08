19b27eafdf422ca320b257be776ee32e3a7a9b54
Hossain Ayman <ayman.dev2003@gmail.com>
2026-07-08 18:41:23 +0600

    Added CRUD support in DAO and Service

--- src/main/java/com/employeeManagementCRUD/EmployeeDirectory/DAO/EmployeeDAOjpaImpl.java
+++ src/main/java/com/employeeManagementCRUD/EmployeeDirectory/DAO/EmployeeDAOjpaImpl.java
@@ -19,4 +19,29 @@ public class EmployeeDAOjpaImpl implements EmployeeDAO {
         TypedQuery<Employee> query = entityManager.createQuery("from Employee", Employee.class);
         return  query.getResultList();
     }
+
+    @Override
+    public Employee findById(int id) {
+        Employee dbEmployee = entityManager.find(Employee.class, id);
+        return dbEmployee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        Employee dbEmployee = entityManager.merge(employee);
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int id ) {
+       Employee dbEmployee = entityManager.find(Employee.class,id);
+    }
+
+    @Override
+    public List<Employee> findByRole(String role) {
+        TypedQuery<Employee> theQuery = entityManager.createQuery("from Employee where role = :role", Employee.class);
+        theQuery.setParameter("role", role);
+        List<Employee> theEmployees = theQuery.getResultList();
+        return theEmployees;
+    }
 }
--- src/main/java/com/employeeManagementCRUD/EmployeeDirectory/Service/EmployeeServiceImpl.java
+++ src/main/java/com/employeeManagementCRUD/EmployeeDirectory/Service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.employeeManagementCRUD.EmployeeDirectory.Service;
 
 import com.employeeManagementCRUD.EmployeeDirectory.DAO.EmployeeDAO;
 import com.employeeManagementCRUD.EmployeeDirectory.Entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +19,27 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<Employee> findAll() {
         return employeeDAO.findAll();
     }
+
+    @Override
+    public Employee findById(int id) {
+       return employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return  employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int id) {
+      employeeDAO.deleteById(id);
+    }
+
+    @Override
+    public List<Employee> findByRole(String role) {
+        return employeeDAO.findByRole(role);
+    }
+
 }
